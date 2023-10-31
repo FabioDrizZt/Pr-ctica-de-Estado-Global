ad058a72492dd6fdbab8771e272c25ec4f35377a
fabio <fabio@digitalmind.com>
2023-10-31 18:01:02 -0300

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import LanguageContext from '../context'
+
+const Body = () => {
+  /* CONSEJO: Utilice useContext() */
+
+  return (
+    <div>
+      {/* CONSEJO: Utilice los valores capturados a través del contexto */}
+      <h1>Título</h1>
+      <p>Descrição</p>
+    </div>
+  )
+}
+
+export default Body
